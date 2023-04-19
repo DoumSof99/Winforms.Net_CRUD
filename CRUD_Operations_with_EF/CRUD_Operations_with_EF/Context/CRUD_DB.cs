b29797b04c47255ab783f62f5ea4d6dc5903c8d0
DoumSof99 <sofiadmn99@gmail.com>
2023-04-19 17:57:21 +0300

    created architecture, installed packs for EF and conn to DB

---
+++ CRUD_Operations_with_EF/CRUD_Operations_with_EF/Context/CRUD_DB.cs
@@ -0,0 +1,20 @@
+﻿using CRUD_Operations_with_EF.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Operations_with_EF.Context
+{
+    public class CRUD_DB: DbContext
+    {
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=PersonCRUDandEF_DB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ CRUD_Operations_with_EF/CRUD_Operations_with_EF/Services/Repository.cs
@@ -0,0 +1,32 @@
+﻿using CRUD_Operations_with_EF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Operations_with_EF.Services
+{
+    public class Repository
+    {
+        //public List<Person> Read(string FirstName, string LastName)
+        //{
+
+        //}
+        public void Create(string FirstName, string LastName)
+        {
+            
+        }
+
+        public void Update(string FirstName, string LastName)
+        {
+
+        }
+
+        public void Delete(string FirstName, string LastName)
+        {
+
+        }
+
+    }
+}
