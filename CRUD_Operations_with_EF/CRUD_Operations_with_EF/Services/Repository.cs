d63d7f5f15a293092b22f4bf32a5186483076f2e
DoumSof99 <sofiadmn99@gmail.com>
2023-04-19 18:10:15 +0300

    created CRUD Methods / manipulation to DB and reverse

--- CRUD_Operations_with_EF/CRUD_Operations_with_EF/Services/Repository.cs
+++ CRUD_Operations_with_EF/CRUD_Operations_with_EF/Services/Repository.cs
@@ -1,4 +1,5 @@
-﻿using CRUD_Operations_with_EF.Model;
+﻿using CRUD_Operations_with_EF.Context;
+using CRUD_Operations_with_EF.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,23 +10,59 @@ namespace CRUD_Operations_with_EF.Services
 {
     public class Repository
     {
-        //public List<Person> Read(string FirstName, string LastName)
-        //{
-
-        //}
-        public void Create(string FirstName, string LastName)
+        public List<Person> Read(string firstName, string lastName, int age)
         {
-            
+            using (CRUD_DB db = new())
+            {
+                return db.People.ToList();
+            }
+        }
+        public void Create(string firstName, string lastName, int age)
+        {
+            using (CRUD_DB db = new())
+            {
+                Person person = new Person()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age
+                };
+
+                db.People.Add(person);
+                db.SaveChanges();
+            }
         }
 
-        public void Update(string FirstName, string LastName)
+        public void Update(int id, string firstName, string lastName, int age)
         {
+            using (CRUD_DB db = new())
+            {
+                Person person = db.People.Find(id);
 
+                if (person != null)
+                {
+                    person.FirstName = firstName;
+                    person.LastName = lastName; 
+                    person.Age = age;   
+                    db.SaveChanges();
+
+                }
+            }
         }
 
-        public void Delete(string FirstName, string LastName)
+        public void Delete(int id)
         {
+            using (CRUD_DB db = new())
+            {
+                Person idPerson = db.People.Find(id);
 
+                if (idPerson != null)
+                {
+                    db.Remove(id);
+                    db.SaveChanges();
+
+                }
+            }
         }
 
     }
